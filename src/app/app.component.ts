682dc3fbc468b117583c93d2ba947a641b00fc9a
Alexander Karrer <karrer.alexander@outlook.com>
2024-03-28 07:20:34 +0100

    drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray} from '@angular/cdk/drag-drop';
+import {Phone, PhoneService} from "../phone.service";
+import {NgForOf, NgIf} from "@angular/common";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, CdkDrag, NgForOf, CdkDropList],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'drag-drop-template';
+
+  allPhones:Phone[] = [];
+  phoneOrder:Map<number,number> = new Map();
+
+  sortedPhones: Phone[] = [];
+
+  constructor(private phoneService:PhoneService) {
+  }
+
+  ngOnInit(): void {
+    this.loadPhoneOrder();
+    this.loadPhones();
+    this.sortPhones();
+  }
+
+  loadPhones() {
+    this.allPhones = this.phoneService.getAllPhones();
+  }
+
+  loadPhoneOrder() {
+    this.phoneOrder = this.phoneService.getPhoneOrder();
+  }
+
+  sortPhones(): void {
+    this.sortedPhones = this.allPhones.sort((a, b) => this.phoneOrder.get(a.id)! - this.phoneOrder.get(b.id)!);
+  }
+
+  drop(event: CdkDragDrop<Phone[]>): void {
+    moveItemInArray(this.sortedPhones, event.previousIndex, event.currentIndex);
+    this.sortedPhones.forEach((phone, index) => {
+      this.phoneOrder.set(phone.id, index);
+    });
+  }
 }
